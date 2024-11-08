68f120fe1cfadb4d8fc64a94741a88a8cc015fc4
micke <gekim360@gmail.com>
2024-11-07 21:05:09 -0500

    Add files via upload

---
+++ test2.bat
@@ -0,0 +1,3 @@
+selenium-side-runner --server http://192.168.56.1:4444/wd/hub -c "browserName=chrome"  RegistrationMercuryCounFixedSu.side
+selenium-side-runner --server http://192.168.56.1:4444/wd/hub -c "browserName=firefox"  RegistrationMercuryCounFixedSu.side
+
